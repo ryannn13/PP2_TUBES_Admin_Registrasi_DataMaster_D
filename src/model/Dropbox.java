236a3c91d9f49fe74ea5bc4b8bb6b5d7d3323d60
ryannn13 <ryan.hidayat0604@gmail.com>
2025-01-11 22:39:39 +0700

    Update - Penjemputan dan Dropbox

--- src/model/Dropbox.java
+++ src/model/Dropbox.java
@@ -1,5 +1,40 @@
 package model;
 
 public class Dropbox {
-    
+    private int dropboxId;
+    private String lokasi;
+    private int kapasitas;
+
+    public Dropbox() {
+    }
+
+    public Dropbox(String lokasi, int kapasitas) {
+        this.lokasi = lokasi;
+        this.kapasitas = kapasitas;
+    }
+
+    // Getters and Setters
+    public int getDropboxId() {
+        return dropboxId;
+    }
+
+    public void setDropboxId(int dropboxId) {
+        this.dropboxId = dropboxId;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public int getKapasitas() {
+        return kapasitas;
+    }
+
+    public void setKapasitas(int kapasitas) {
+        this.kapasitas = kapasitas;
+    }
 }
--- src/model/Points.java
+++ src/model/Points.java
@@ -1,5 +1,51 @@
 package model;
 
+import java.util.Date;
+
 public class Points {
-    
+    private int pointId;
+    private int masyarakatId;
+    private int jumlah;
+    private Date tanggal;
+
+    public Points() {
+    }
+
+    public Points(int masyarakatId, int jumlah) {
+        this.masyarakatId = masyarakatId;
+        this.jumlah = jumlah;
+    }
+
+    // Getters and Setters
+    public int getPointId() {
+        return pointId;
+    }
+
+    public void setPointId(int pointId) {
+        this.pointId = pointId;
+    }
+
+    public int getMasyarakatId() {
+        return masyarakatId;
+    }
+
+    public void setMasyarakatId(int masyarakatId) {
+        this.masyarakatId = masyarakatId;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public Date getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(Date tanggal) {
+        this.tanggal = tanggal;
+    }
 }
